c9e0b64132ab583a353768e1d7c94eaaf14edfb0
Frans Bouma <frans@sd.nl>
2015-04-22 16:41:54 +0200

    Front page design is now done.

---
+++ Design/js/general.js
@@ -0,0 +1,36 @@
+﻿/* general javascript for site functionality */
+// initialize the tooltips
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip();
+});
+
+// to flip the shadow on the navbar after scrolling. 
+$(window).scroll(function () {
+	if ($(this).scrollTop() === 0) {
+		$('#main-header').removeClass("headerscrolled");
+		$('#main-header').addClass("headerattop");
+	} else {
+		$('#main-header').addClass("headerscrolled");
+		$('#main-header').removeClass("headerattop");
+	}
+});
+
+// for remembering the collapse state of panels that can be collapsed. Stored in local storage. 
+$(document).ready(function () {
+	$('.collapse')
+		.on('hidden.bs.collapse', function () {
+			if (this.id) {
+				localStorage[this.id] = 'true';
+			}
+		})
+		.on('shown.bs.collapse', function () {
+			if (this.id) {
+				localStorage.removeItem(this.id);
+			}
+		})
+		.each(function () {
+			if (this.id && localStorage[this.id] === 'true') {
+				$(this).collapse('hide');
+			}
+		});
+});
