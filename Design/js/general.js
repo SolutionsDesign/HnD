c25477e411604fd152a18b4260bb5fb85334a0e0
Frans Bouma <frans@sd.nl>
2015-04-28 17:41:25 +0200

    further work on forum page

--- Design/js/general.js
+++ Design/js/general.js
@@ -34,3 +34,10 @@ $(document).ready(function () {
 			}
 		});
 });
+
+$('.dropdown').dropdown().on("hide.bs.dropdown", function (e) {
+	if ($.contains(dropdown, e.target)) {
+		e.preventDefault();
+		//or return false;
+	}
+});
