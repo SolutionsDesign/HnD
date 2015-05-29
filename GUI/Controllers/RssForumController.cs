166a75dd02af4a482ea832db2545444c7353fa29
Frans Bouma <frans@sd.nl>
2015-05-29 17:40:36 +0200

    Fixed the razor issue with the rssforum view, now ported to MVC
    Kept the rssforum.aspx around to redirect to the mvc version.

---
+++ GUI/Controllers/RssForumController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SD.HnD.BL;
+using SD.HnD.Gui.Models;
+
+namespace SD.HnD.Gui.Controllers
+{
+    public class RssForumController : Controller
+    {
+        // GET: RssForum
+        public ActionResult Index(int id=0)
+        {
+			Response.ContentType = "text/xml";
+	        var forum = CacheManager.GetForum(id);
+	        if(forum == null)
+	        {
+		        return View();
+	        }
+
+	        var data = new RssForumData()
+					   {
+						   ForumUrl = "http://" + Request.Url.Host + ApplicationAdapter.GetVirtualRoot() + @"/Forum/" + id,
+						   SiteName = ApplicationAdapter.GetSiteName(),
+						   ForumName = forum.ForumName,
+						   ForumItems = ForumGuiHelper.GetLastPostedMessagesInForum(10, id)
+					   };
+
+            return View(data);
+        }
+    }
+}
---
+++ GUI/Models/RssForumData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SD.HnD.DAL.TypedListClasses;
+
+namespace SD.HnD.Gui.Models
+{
+	public class RssForumData
+	{
+		public string SiteName { get; set; }
+		public string ForumUrl { get; set; }
+		public string ForumName { get; set; }
+		public List<ForumMessagesRow> ForumItems { get; set; }
+	}
+}
