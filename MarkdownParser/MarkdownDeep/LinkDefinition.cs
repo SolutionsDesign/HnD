c8deec092237665cca1c0d544c7caa30eb919a7e
Frans Bouma <frans@sd.nl>
2016-08-25 14:07:41 +0200

    Added DocNet's MarkdownDeep parser. Nothing changed yet. For #5

---
+++ MarkdownParser/MarkdownDeep/LinkDefinition.cs
@@ -0,0 +1,373 @@
+﻿// 
+//   MarkdownDeep - http://www.toptensoftware.com/markdowndeep
+//	 Copyright (C) 2010-2011 Topten Software
+// 
+//   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this product except in 
+//   compliance with the License. You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software distributed under the License is 
+//   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+//   See the License for the specific language governing permissions and limitations under the License.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarkdownDeep
+{
+	public class LinkDefinition
+	{
+		public LinkDefinition(string id) : this(id, string.Empty, string.Empty)
+		{
+		}
+
+		public LinkDefinition(string id, string url) : this(id, url, string.Empty)
+		{
+		}
+
+		public LinkDefinition(string id, string url, string title)
+		{
+			this.Id = id;
+			this.Url = url;
+			this.Title = title;
+		}
+
+
+		internal void RenderLink(Markdown m, StringBuilder b, string link_text, List<string> specialAttributes)
+		{
+			if (this.Url.StartsWith("mailto:"))
+			{
+				b.Append("<a href=\"");
+				Utils.HtmlRandomize(b, this.Url);
+				b.Append('\"');
+				if (!String.IsNullOrEmpty(this.Title))
+				{
+					b.Append(" title=\"");
+					Utils.SmartHtmlEncodeAmpsAndAngles(b, this.Title);
+					b.Append('\"');
+				}
+				b.Append('>');
+				Utils.HtmlRandomize(b, link_text);
+				b.Append("</a>");
+			}
+			else
+			{
+				HtmlTag tag = new HtmlTag("a");
+
+				// encode url
+				StringBuilder sb = m.GetStringBuilder();
+				Utils.SmartHtmlEncodeAmpsAndAngles(sb, this.Url);
+				tag.attributes["href"] = sb.ToString();
+
+				// encode title
+				if (!String.IsNullOrEmpty(this.Title ))
+				{
+					sb.Length = 0;
+					Utils.SmartHtmlEncodeAmpsAndAngles(sb, this.Title);
+					tag.attributes["title"] = sb.ToString();
+				}
+
+				if(specialAttributes.Any())
+				{
+					LinkDefinition.HandleSpecialAttributes(specialAttributes, sb, tag);
+				}
+
+				// Do user processing
+				m.OnPrepareLink(tag);
+
+				// Render the opening tag
+				tag.RenderOpening(b);
+
+				b.Append(link_text);	  // Link text already escaped by SpanFormatter
+				b.Append("</a>");
+			}
+		}
+
+
+		internal void RenderImg(Markdown m, StringBuilder b, string alt_text, List<string> specialAttributes)
+		{
+			HtmlTag tag = new HtmlTag("img");
+
+			// encode url
+			StringBuilder sb = m.GetStringBuilder();
+			Utils.SmartHtmlEncodeAmpsAndAngles(sb, Url);
+			tag.attributes["src"] = sb.ToString();
+
+			// encode alt text
+			if (!String.IsNullOrEmpty(alt_text))
+			{
+				sb.Length = 0;
+				Utils.SmartHtmlEncodeAmpsAndAngles(sb, alt_text);
+				tag.attributes["alt"] = sb.ToString();
+			}
+
+			// encode title
+			if (!String.IsNullOrEmpty(Title))
+			{
+				sb.Length = 0;
+				Utils.SmartHtmlEncodeAmpsAndAngles(sb, Title);
+				tag.attributes["title"] = sb.ToString();
+			}
+			if(specialAttributes.Any())
+			{
+				LinkDefinition.HandleSpecialAttributes(specialAttributes, sb, tag);
+			}
+			tag.closed = true;
+
+			m.OnPrepareImage(tag, m.RenderingTitledImage);
+
+			tag.RenderOpening(b);
+		}
+
+
+		// Parse a link definition from a string (used by test cases)
+		internal static LinkDefinition ParseLinkDefinition(string str, bool ExtraMode)
+		{
+			StringScanner p = new StringScanner(str);
+			return ParseLinkDefinitionInternal(p, ExtraMode);
+		}
+
+		// Parse a link definition
+		internal static LinkDefinition ParseLinkDefinition(StringScanner p, bool ExtraMode)
+		{
+			int savepos=p.Position;
+			var l = ParseLinkDefinitionInternal(p, ExtraMode);
+			if (l==null)
+				p.Position = savepos;
+			return l;
+
+		}
+
+		internal static LinkDefinition ParseLinkDefinitionInternal(StringScanner p, bool ExtraMode)
+		{
+			// Skip leading white space
+			p.SkipWhitespace();
+
+			// Must start with an opening square bracket
+			if (!p.SkipChar('['))
+				return null;
+
+			// Extract the id
+			p.Mark();
+			if (!p.Find(']'))
+				return null;
+			string id = p.Extract();
+			if (id.Length == 0)
+				return null;
+			if (!p.SkipString("]:"))
+				return null;
+
+			// Parse the url and title
+			var link=ParseLinkTarget(p, id, ExtraMode);
+
+			// and trailing whitespace
+			p.SkipLinespace();
+
+			// Trailing crap, not a valid link reference...
+			if (!p.Eol)
+				return null;
+
+			return link;
+		}
+
+		// Parse just the link target
+		// For reference link definition, this is the bit after "[id]: thisbit"
+		// For inline link, this is the bit in the parens: [link text](thisbit)
+		internal static LinkDefinition ParseLinkTarget(StringScanner p, string id, bool ExtraMode)
+		{
+			// Skip whitespace
+			p.SkipWhitespace();
+
+			// End of string?
+			if (p.Eol)
+				return null;
+
+			// Create the link definition
+			var r = new LinkDefinition(id);
+
+			// Is the url enclosed in angle brackets
+			if (p.SkipChar('<'))
+			{
+				// Extract the url
+				p.Mark();
+
+				// Find end of the url
+				while (p.Current != '>')
+				{
+					if (p.Eof)
+						return null;
+					p.SkipEscapableChar(ExtraMode);
+				}
+
+				string url = p.Extract();
+				if (!p.SkipChar('>'))
+					return null;
+
+				// Unescape it
+				r.Url = Utils.UnescapeString(url.Trim(), ExtraMode);
+			}
+			else
+			{
+				// Find end of the url
+				p.Mark();
+				int paren_depth = 1;
+				while (!p.Eol)
+				{
+					char ch=p.Current;
+					if (char.IsWhiteSpace(ch))
+						break;
+					if (id == null)
+					{
+						if (ch == '(')
+							paren_depth++;
+						else if (ch == ')')
+						{
+							paren_depth--;
+							if (paren_depth==0)
+								break;
+						}
+					}
+
+					p.SkipEscapableChar(ExtraMode);
+				}
+
+				r.Url = Utils.UnescapeString(p.Extract().Trim(), ExtraMode);
+			}
+
+			p.SkipLinespace();
+
+			// End of inline target
+			if (p.DoesMatch(')'))
+				return r;
+
+			bool bOnNewLine = p.Eol;
+			int posLineEnd = p.Position;
+			if (p.Eol)
+			{
+				p.SkipEol();
+				p.SkipLinespace();
+			}
+
+			// Work out what the title is delimited with
+			char delim;
+			switch (p.Current)
+			{
+				case '\'':  
+				case '\"':
+					delim = p.Current;
+					break;
+
+				case '(':
+					delim = ')';
+					break;
+
+				default:
+					if (bOnNewLine)
+					{
+						p.Position = posLineEnd;
+						return r;
+					}
+					else
+						return null;
+			}
+
+			// Skip the opening title delimiter
+			p.SkipForward(1);
+
+			// Find the end of the title
+			p.Mark();
+			while (true)
+			{
+				if (p.Eol)
+					return null;
+
+				if (p.Current == delim)
+				{
+
+					if (delim != ')')
+					{
+						int savepos = p.Position;
+
+						// Check for embedded quotes in title
+
+						// Skip the quote and any trailing whitespace
+						p.SkipForward(1);
+						p.SkipLinespace();
+
+						// Next we expect either the end of the line for a link definition
+						// or the close bracket for an inline link
+						if ((id == null && p.Current != ')') ||
+							(id != null && !p.Eol))
+						{
+							continue;
+						}
+
+						p.Position = savepos;
+					}
+
+					// End of title
+					break;
+				}
+
+				p.SkipEscapableChar(ExtraMode);
+			}
+
+			// Store the title
+			r.Title = Utils.UnescapeString(p.Extract(), ExtraMode);
+
+			// Skip closing quote
+			p.SkipForward(1);
+
+			// Done!
+			return r;
+		}
+
+
+		private static void HandleSpecialAttributes(List<string> specialAttributes, StringBuilder sb, HtmlTag tag)
+		{
+			string id = specialAttributes.FirstOrDefault(s => s.StartsWith("#"));
+			if(id != null && id.Length > 1)
+			{
+				sb.Length = 0;
+				Utils.SmartHtmlEncodeAmpsAndAngles(sb, id.Substring(1));
+				tag.attributes["id"] = sb.ToString();
+			}
+			var cssClasses = new List<string>();
+			foreach(var cssClass in specialAttributes.Where(s => s.StartsWith(".") && s.Length > 1))
+			{
+				sb.Length = 0;
+				Utils.SmartHtmlEncodeAmpsAndAngles(sb, cssClass.Substring(1));
+				cssClasses.Add(sb.ToString());
+			}
+			if(cssClasses.Any())
+			{
+				tag.attributes["class"] = string.Join(" ", cssClasses.ToArray());
+			}
+			foreach(var nameValuePair in specialAttributes.Where(s => s.Contains("=") && s.Length > 2 && !s.StartsWith(".") && !s.StartsWith("#")))
+			{
+				var pair = nameValuePair.Split('=');
+				if(pair.Length == 2)
+				{
+					sb.Length = 0;
+					Utils.SmartHtmlEncodeAmpsAndAngles(sb, pair[0]);
+					var key = sb.ToString();
+					sb.Length = 0;
+					Utils.SmartHtmlEncodeAmpsAndAngles(sb, pair[1]);
+					var value = sb.ToString();
+					tag.attributes[key] = value;
+				}
+			}
+		}
+
+
+		#region Properties
+		public string Id { get; set; }
+		public string Url { get; set; }
+		public string Title { get; set; }
+		#endregion
+	}
+}
---
+++ MarkdownParser/MarkdownDeep/StringScanner.cs
@@ -0,0 +1,569 @@
+﻿// 
+//   MarkdownDeep - http://www.toptensoftware.com/markdowndeep
+//	 Copyright (C) 2010-2011 Topten Software
+// 
+//   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this product except in 
+//   compliance with the License. You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software distributed under the License is 
+//   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+//   See the License for the specific language governing permissions and limitations under the License.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarkdownDeep
+{
+	/*
+	 * StringScanner is a simple class to help scan through an input string.
+	 * 
+	 * Maintains a current position with various operations to inspect the current
+	 * character, skip forward, check for matches, skip whitespace etc...
+	 */
+	public class StringScanner
+	{
+		// Constructor
+		public StringScanner()
+		{
+		}
+
+		// Constructor
+		public StringScanner(string str)
+		{
+			Reset(str);
+		}
+
+		// Constructor
+		public StringScanner(string str, int pos)
+		{
+			Reset(str, pos);
+		}
+
+		// Constructor
+		public StringScanner(string str, int pos, int len)
+		{
+			Reset(str, pos, len);
+		}
+
+		// Reset
+		public void Reset(string str)
+		{
+			Reset(str, 0, str!=null ? str.Length : 0);
+		}
+
+		// Reset
+		public void Reset(string str, int pos)
+		{
+			Reset(str, pos, str!=null ? str.Length - pos : 0);
+		}
+
+		// Reset
+		public void Reset(string str, int pos, int len)
+		{
+			if (str == null)
+				str = "";
+			if (len < 0)
+				len = 0;
+			if (pos < 0)
+				pos = 0;
+			if (pos > str.Length)
+				pos = str.Length;
+
+			this.str = str;
+			this.start = pos;
+			this.pos = pos;
+			this.end = pos + len;
+
+			if (end > str.Length)
+				end = str.Length;
+		}
+
+		// Get the entire input string
+		public string Input
+		{
+			get
+			{
+				return str;
+			}
+		}
+
+		// Get the character at the current position
+		public char Current
+		{
+			get
+			{
+				if (pos < start || pos >= end)
+					return '\0';
+				else
+					return str[pos];
+			}
+		}
+
+		// Get/set the current position
+		public int Position
+		{
+			get
+			{
+				return pos;
+			}
+			set
+			{
+				pos = value;
+			}
+		}
+
+		// Get the remainder of the input 
+		// (use this in a watch window while debugging :)
+		public string Remainder
+		{
+			get
+			{
+				return Substring(Position);
+			}
+		}
+
+		// Skip to the end of file
+		public void SkipToEof()
+		{
+			pos = end;
+		}
+
+
+		// Skip to the end of the current line
+		public void SkipToEol()
+		{
+			while (pos < end)
+			{
+				char ch=str[pos];
+				if (ch=='\r' || ch=='\n')
+					break;
+				pos++;
+			}
+		}
+
+		// Skip if currently at a line end
+		public bool SkipEol()
+		{
+			if (pos < end)
+			{
+				char ch = str[pos];
+				if (ch == '\r')
+				{
+					pos++;
+					if (pos < end && str[pos] == '\n')
+						pos++;
+					return true;
+				}
+
+				else if (ch == '\n')
+				{
+					pos++;
+					if (pos < end && str[pos] == '\r')
+						pos++;
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		// Skip to the next line
+		public void SkipToNextLine()
+		{
+			SkipToEol();
+			SkipEol();
+		}
+
+		// Get the character at offset from current position
+		// Or, \0 if out of range
+		public char CharAtOffset(int offset)
+		{
+			int index = pos + offset;
+			
+			if (index < start)
+				return '\0';
+			if (index >= end)
+				return '\0';
+			return str[index];
+		}
+
+		// Skip a number of characters
+		public void SkipForward(int characters)
+		{
+			pos += characters;
+		}
+
+		// Skip a character if present
+		public bool SkipChar(char ch)
+		{
+			if (Current == ch)
+			{
+				SkipForward(1);
+				return true;
+			}
+
+			return false;	
+		}
+
+		// Skip a matching string
+		public bool SkipString(string str)
+		{
+			if (DoesMatch(str))
+			{
+				SkipForward(str.Length);
+				return true;
+			}
+
+			return false;
+		}
+
+		// Skip a matching string
+		public bool SkipStringI(string str)
+		{
+			if (DoesMatchI(str))
+			{
+				SkipForward(str.Length);
+				return true;
+			}
+
+			return false;
+		}
+
+		// Skip any whitespace
+		public bool SkipWhitespace()
+		{
+			if (!char.IsWhiteSpace(Current))
+				return false;
+			SkipForward(1);
+
+			while (char.IsWhiteSpace(Current))
+				SkipForward(1);
+
+			return true;
+		}
+
+		// Check if a character is space or tab
+		public static bool IsLineSpace(char ch)
+		{
+			return ch == ' ' || ch == '\t';
+		}
+
+		// Skip spaces and tabs
+		public bool SkipLinespace()
+		{
+			if (!IsLineSpace(Current))
+				return false;
+			SkipForward(1);
+
+			while (IsLineSpace(Current))
+				SkipForward(1);
+
+			return true;
+		}
+
+		// Does current character match something
+		public bool DoesMatch(char ch)
+		{
+			return Current == ch;
+		}
+
+		// Does character at offset match a character
+		public bool DoesMatch(int offset, char ch)
+		{
+			return CharAtOffset(offset) == ch;
+		}
+
+		// Does current character match any of a range of characters
+		public bool DoesMatchAny(char[] chars)
+		{
+			for (int i = 0; i < chars.Length; i++)
+			{
+				if (DoesMatch(chars[i]))
+					return true;
+			}
+			return false;
+		}
+
+		// Does current character match any of a range of characters
+		public bool DoesMatchAny(int offset, char[] chars)
+		{
+			for (int i = 0; i < chars.Length; i++)
+			{
+				if (DoesMatch(offset, chars[i]))
+					return true;
+			}
+			return false;
+		}
+
+		// Does current string position match a string
+		public bool DoesMatch(string str)
+		{
+			for (int i = 0; i < str.Length; i++)
+			{
+				if (str[i] != CharAtOffset(i))
+					return false;
+			}
+			return true;
+		}
+
+		// Does current string position match a string
+		public bool DoesMatchI(string str)
+		{
+			return string.Compare(str, Substring(Position, str.Length), true) == 0;
+		}
+
+		// Extract a substring
+		public string Substring(int start)
+		{
+			return str.Substring(start, end-start);
+		}
+
+		// Extract a substring
+		public string Substring(int start, int len)
+		{
+			if (start + len > end)
+				len = end - start;
+
+			return str.Substring(start, len);
+		}
+
+		// Scan forward for a character
+		public bool Find(char ch)
+		{
+			if (pos >= end)
+				return false;
+
+			// Find it
+			int index = str.IndexOf(ch, pos);
+			if (index < 0 || index>=end)
+				return false;
+
+			// Store new position
+			pos = index;
+			return true;
+		}
+
+		// Find any of a range of characters
+		public bool FindAny(char[] chars)
+		{
+			if (pos >= end)
+				return false;
+
+			// Find it
+			int index = str.IndexOfAny(chars, pos);
+			if (index < 0 || index>=end)
+				return false;
+
+			// Store new position
+			pos = index;
+			return true;
+		}
+
+		// Forward scan for a string
+		public bool Find(string find)
+		{
+			if (pos >= end)
+				return false;
+
+			int index = str.IndexOf(find, pos);
+			if (index < 0 || index > end-find.Length)
+				return false;
+
+			pos = index;
+			return true;
+		}
+
+		// Forward scan for a string (case insensitive)
+		public bool FindI(string find)
+		{
+			if (pos >= end)
+				return false;
+
+			int index = str.IndexOf(find, pos, StringComparison.InvariantCultureIgnoreCase);
+			if (index < 0 || index >= end - find.Length)
+				return false;
+
+			pos = index;
+			return true;
+		}
+
+		// Are we at eof?
+		public bool Eof
+		{
+			get
+			{
+				return pos >= end;
+			}
+		}
+
+		// Are we at eol?
+		public bool Eol
+		{
+			get
+			{
+				return IsLineEnd(Current);
+			}
+		}
+
+		// Are we at bof?
+		public bool Bof
+		{
+			get
+			{
+				return pos == start;
+			}
+		}
+
+		// Mark current position
+		public void Mark()
+		{
+			mark = pos;
+		}
+
+		// Extract string from mark to current position
+		public string Extract()
+		{
+			if (mark >= pos)
+				return "";
+
+			return str.Substring(mark, pos - mark);
+		}
+
+		// Skip an identifier
+		public bool SkipIdentifier(ref string identifier)
+		{
+			int savepos = Position;
+			if (!Utils.ParseIdentifier(this.str, ref pos, ref identifier))
+				return false;
+			if (pos >= end)
+			{
+				pos = savepos;
+				return false;
+			}
+			return true;
+		}
+
+		public bool SkipFootnoteID(out string id)
+		{
+			int savepos = Position;
+
+			SkipLinespace();
+
+			Mark();
+
+			while (true)
+			{
+				char ch = Current;
+				if (char.IsLetterOrDigit(ch) || ch == '-' || ch == '_' || ch == ':' || ch == '.' || ch == ' ')
+					SkipForward(1);
+				else
+					break;
+			}
+
+			if (Position > mark)
+			{
+				id = Extract().Trim();
+				if (!String.IsNullOrEmpty(id))
+				{
+					SkipLinespace();
+					return true;
+				}
+			}
+
+			Position = savepos;
+			id = null;
+			return false;
+		}
+
+		// Skip a Html entity (eg: &amp;)
+		public bool SkipHtmlEntity(ref string entity)
+		{
+			int savepos = Position;
+			if (!Utils.SkipHtmlEntity(this.str, ref pos, ref entity))
+				return false;
+			if (pos > end)
+			{
+				pos = savepos;
+				return false;
+			}
+			return true;
+		}
+
+		// Check if a character marks end of line
+		public static bool IsLineEnd(char ch)
+		{
+			return ch == '\r' || ch == '\n' || ch=='\0';
+		}
+
+		bool IsUrlChar(char ch)
+		{
+			switch (ch)
+			{
+				case '+':
+				case '&':
+				case '@':
+				case '#':
+				case '/':
+				case '%':
+				case '?':
+				case '=':
+				case '~':
+				case '_':
+				case '|':
+				case '[':
+				case ']':
+				case '(':
+				case ')':
+				case '!':
+				case ':':
+				case ',':
+				case '.':
+				case ';':
+					return true;
+
+				default:
+					return Char.IsLetterOrDigit(ch);
+			}
+		}
+
+		// Attributes
+		string str;
+		int start;
+		int pos;
+		int end;
+		int mark;
+
+
+		/// <summary>
+		/// Unskips the CRLF before position. This simply means it returns the new position calculated from the specified position if before the specified
+		/// position a CRLF is present.
+		/// </summary>
+		/// <param name="position">The position.</param>
+		/// <returns>position minus 0, 1 or 2 depending on whether a CRLF is present right before position: if so, it returns position - the length of the CRLF
+		/// which can be 1 or 2 depending on the fact whether it's \r or \n or both.</returns>
+		protected int UnskipCRLFBeforePos(int position)
+		{
+			if(this.Input[position - 1] == '\r' && this.Input[position - 2] == '\n')
+			{
+				position -= 2;
+			}
+			else
+			{
+				if(this.Input[position - 1] == '\n' && this.Input[position - 2] == '\r')
+				{
+					position -= 2;
+				}
+				else
+				{
+					position--;
+				}
+			}
+			return position;
+		}
+	}
+}
