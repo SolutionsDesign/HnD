1fd7485df09e11d105bfc2b4d934f34912d20c75
Frans Bouma <frans@sd.nl>
2021-04-07 11:18:06 +0200

    Version bump

--- BL/Globals.cs
+++ BL/Globals.cs
@@ -30,11 +30,11 @@ namespace SD.HnD.BL
 		/// <summary>
 		/// Version of HnD
 		/// </summary>
-		public static readonly string Version = "3.0";
+		public static readonly string Version = "3.0.1";
 		/// <summary>
 		/// Build of HnD
 		/// </summary>
-		public static readonly string Build = "11-oct-2020";
+		public static readonly string Build = "07-apr-2021";
 		/// <summary>
 		/// Release type of this version
 		/// </summary>
